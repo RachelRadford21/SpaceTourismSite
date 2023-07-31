03ee4f277e140fddf6fdc30df3251728681a45ec
Rachel Radford <rachelradford21@gmail.com>
2023-07-31 17:41:36 -0400

    Initial Commit

---
+++ SpaceTourismSite/SpaceTourismSiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceTourismSiteApp.swift
+//  SpaceTourismSite
+//
+//  Created by Rachel Radford on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct SpaceTourismSiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
